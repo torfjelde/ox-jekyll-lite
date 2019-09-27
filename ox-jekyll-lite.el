46b2a92c7efcae0fecb348d3f594e6ff9117aaa5
Peter Wills <peter.e.wills@gmail.com>
2019-09-27 16:29:37 -0700

    rebrand to jekyll-lite

--- ox-jekyll-lite.el
+++ ox-jekyll-lite.el
@@ -1,4 +1,4 @@
-;;; ox-jekyll-md.el --- Export Jekyll on Markdown articles using org-mode. -*- lexical-binding: t; -*-
+;;; ox-jekyll-lite.el --- Export Jekyll on Markdown articles using org-mode. -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2019 Peter Wills
 
@@ -30,11 +30,11 @@
 ;; Org exporter, based on `md' back-end.
 ;;
 ;; It provides two commands for export, depending on the desired
-;; output: `org-jekyll-md-export-as-md' (temporary buffer) and
-;; `org-jekyll-md-export-to-md' ("md" file with YAML front matter).
+;; output: `org-jekyll-lite-export-as-md' (temporary buffer) and
+;; `org-jekyll-lite-export-to-md' ("md" file with YAML front matter).
 ;;
-;; For publishing, `org-jekyll-md-publish-to-md' is available.
-;; For composing, `org-jekyll-md-insert-export-options-template' is available.
+;; For publishing, `org-jekyll-lite-publish-to-md' is available.
+;; For composing, `org-jekyll-lite-insert-export-options-template' is available.
 
 (require 'ox-md)
 (require 's)
@@ -42,13 +42,13 @@
 
 ;;; User Configurable Variables
 
-(defgroup org-export-jekyll-md nil
+(defgroup org-export-jekyll-lite nil
   "Options for exporting Org mode files to jekyll MD."
   :tag "Org Jekyll MD"
   :group 'org-export
   :version "24.2")
 
-(defcustom org-jekyll-md-include-yaml-front-matter t
+(defcustom org-jekyll-lite-include-yaml-front-matter t
   "If true, then include yaml-front-matter when exporting to md.
 
 If false, then you should include the yaml front matter like this at the top of
@@ -67,17 +67,17 @@ excerpt: Details on exporting org files into a jekyll-friendly markdown format.
   :group 'org-export-jekyll
   :type 'boolean)
 
-(defcustom org-jekyll-md-layout ""
+(defcustom org-jekyll-lite-layout ""
   "Default layout used in Jekyll article."
   :group 'org-export-jekyll
   :type 'string)
 
-(defcustom org-jekyll-md-categories "post"
+(defcustom org-jekyll-lite-categories "post"
   "Default space-separated categories in Jekyll article."
   :group 'org-export-jekyll
   :type 'string)
 
-(defcustom org-jekyll-md-tags ""
+(defcustom org-jekyll-lite-tags ""
   "Default space-separated tags in Jekyll article."
   :group 'org-export-jekyll
   :type 'string)
@@ -90,33 +90,33 @@ excerpt: Details on exporting org files into a jekyll-friendly markdown format.
 ;;; Define Back-End
 
 (org-export-define-derived-backend 'jekyll 'md
-  :filters-alist '((:filter-parse-tree . org-jekyll-md-separate-elements))
+  :filters-alist '((:filter-parse-tree . org-jekyll-lite-separate-elements))
   :menu-entry
   '(?j "Jekyll: export to Markdown with YAML front matter."
-       ((?J "As Jekyll buffer" (lambda (a s v b) (org-jekyll-md-export-as-md a s v)))
-        (?j "As Jekyll file" (lambda (a s v b) (org-jekyll-md-export-to-md a s v)))))
+       ((?J "As Jekyll buffer" (lambda (a s v b) (org-jekyll-lite-export-as-md a s v)))
+        (?j "As Jekyll file" (lambda (a s v b) (org-jekyll-lite-export-to-md a s v)))))
   :translate-alist
-  '((headline . org-jekyll-md-headline-offset)
-    (inner-template . org-jekyll-md-inner-template) 
-    (footnote-reference . org-jekyll-md-footnote-reference)
-    (src-block . org-jekyll-md-src-block)
-    (latex-environment . org-jekyll-md-latex-environment)
-    (latex-fragment . org-jekyll-md-latex-fragment)
-    (link . org-jekyll-md-link)
-    (underline . org-jekyll-md-underline)
-    (table . org-jekyll-md-table)
-    (table-cell . org-jekyll-md-table-cell)
-    (table-row . org-jekyll-md-table-row)
-    (template . org-jekyll-md-template)) ;; add YAML front matter.
+  '((headline . org-jekyll-lite-headline-offset)
+    (inner-template . org-jekyll-lite-inner-template) 
+    (footnote-reference . org-jekyll-lite-footnote-reference)
+    (src-block . org-jekyll-lite-src-block)
+    (latex-environment . org-jekyll-lite-latex-environment)
+    (latex-fragment . org-jekyll-lite-latex-fragment)
+    (link . org-jekyll-lite-link)
+    (underline . org-jekyll-lite-underline)
+    (table . org-jekyll-lite-table)
+    (table-cell . org-jekyll-lite-table-cell)
+    (table-row . org-jekyll-lite-table-row)
+    (template . org-jekyll-lite-template)) ;; add YAML front matter.
   :options-alist
-  '((:jekyll-layout "JEKYLL_LAYOUT" nil org-jekyll-md-layout)
-    (:jekyll-categories "JEKYLL_CATEGORIES" nil org-jekyll-md-categories)
-    (:jekyll-tags "JEKYLL_TAGS" nil org-jekyll-md-tags)))
+  '((:jekyll-layout "JEKYLL_LAYOUT" nil org-jekyll-lite-layout)
+    (:jekyll-categories "JEKYLL_CATEGORIES" nil org-jekyll-lite-categories)
+    (:jekyll-tags "JEKYLL_TAGS" nil org-jekyll-lite-tags)))
 
 
 ;;;; Links
 
-(defun org-jekyll-md-resolve-file-path (path)
+(defun org-jekyll-lite-resolve-file-path (path)
   "Resolve a file path so that it treats the jekyll directory as root."
   (if (string= org-jekyll-project-root "")
       (progn
@@ -128,14 +128,14 @@ so file paths will be linked as provided.")
       (s-chop-prefix org-jekyll-project-root-clean path))))
 
 
-(defun org-jekyll-md-file-is-image? (raw-link)
+(defun org-jekyll-lite-file-is-image? (raw-link)
   "Tests whether RAW-LINK is a link to an image file."
   (let* ((image-suffixes '(".apng" ".bmp" ".gif" ".ico" ".cur" ".jpg" ".jpeg" ".jfif"
                           ".pjpeg" ".pjp" ".png" ".svg" ".webp"))
          (suffix-matches (--filter (s-ends-with? it raw-link t) image-suffixes)))
     (not (null suffix-matches))))
 
-(defun org-jekyll-md-link (link desc info)
+(defun org-jekyll-lite-link (link desc info)
   "Transcode a LINK into Markdown format. 
 
 DESC is the links description. 
@@ -161,8 +161,8 @@ Supported link types:
     (cond
      ;; file links. make file paths treat jekyll root as root.
      ((string= type "file")
-      (let* ((jekyll-path (org-jekyll-md-resolve-file-path raw-path)))
-        (if (org-jekyll-md-file-is-image? raw-path)
+      (let* ((jekyll-path (org-jekyll-lite-resolve-file-path raw-path)))
+        (if (org-jekyll-lite-file-is-image? raw-path)
             ;; it's an image, so prepend a bang
             (format "![%s](%s)" desc jekyll-path)
           ;; non-image file, so no bang
@@ -174,15 +174,15 @@ Supported link types:
         (format "[%s](%s)" raw-link raw-link))))))
 
 ;;;; text
-(defun org-jekyll-md-underline (underline contents info)
+(defun org-jekyll-lite-underline (underline contents info)
   "Transcode UNDERLINE from Org to Markdown.
 CONTENTS is the text with underline markup.  INFO is a plist
 holding contextual information."
   (format "<u>%s</u>" contents))
 
 ;;;; Footnote Reference
-(defun org-jekyll-md-footnote-reference (footnote-reference _contents info)
-  "Transcode a FOOTNOTE-REFERENCE element into Jekyll-Md Markdown format.
+(defun org-jekyll-lite-footnote-reference (footnote-reference _contents info)
+  "Transcode a FOOTNOTE-REFERENCE element into Jekyll-Lite Markdown format.
 CONTENTS is nil.  INFO is a plist holding contextual information.
 
 Adapted from ox-blackfriday, via ox-hugo."
@@ -195,7 +195,7 @@ Adapted from ox-blackfriday, via ox-hugo."
    (format "[^fn%d]" (org-export-get-footnote-number footnote-reference info))))
 
 ;;;; Footnote section
-(defun org-jekyll-md-footnote-section (info)
+(defun org-jekyll-lite-footnote-section (info)
   "Format the footnote section.
 INFO is a plist used as a communication channel.
 
@@ -227,7 +227,7 @@ Adapted from ox-blackfriday, via ox-hugo."
                  "\n"))))
 
 ;;;; Latex Environment
-(defun org-jekyll-md-latex-environment (latex-environment _contents info)
+(defun org-jekyll-lite-latex-environment (latex-environment _contents info)
   "Transcode a LATEX-ENVIRONMENT object into Markdown format.
 INFO is a plist holding contextual information.
 
@@ -238,13 +238,13 @@ Adapted from ox-blackfriday via ox-hugo."
       (let* ((latex-env (org-remove-indentation
                          (org-element-property :value latex-environment)))
              (env (org-html-format-latex latex-env 'mathjax info))
-             (env (org-jekyll-md-escape-chars-in-equation env)))
+             (env (org-jekyll-lite-escape-chars-in-equation env)))
         env))
      (t
       (org-html-latex-environment latex-environment nil info)))))
 
 ;;;; Latex Fragment
-(defun org-jekyll-md-latex-fragment (latex-fragment _contents info)
+(defun org-jekyll-lite-latex-fragment (latex-fragment _contents info)
   "Transcode a LATEX-FRAGMENT object into Markdown format.
 INFO is a plist holding contextual information.
 
@@ -254,14 +254,14 @@ Adapted from ox-blackfriday via ox-hugo."
      ((memq processing-type '(t mathjax))
       (let* ((latex-frag (org-element-property :value latex-fragment))
              (frag (org-html-format-latex latex-frag 'mathjax info))
-             (frag (org-jekyll-md-escape-chars-in-equation frag)))
+             (frag (org-jekyll-lite-escape-chars-in-equation frag)))
         ;; (message "[ox-bf-latex-frag DBG] frag: %s" frag)
         frag))
      (t
       (org-html-latex-fragment latex-fragment nil info)))))
 
 ;;;; Escape certain characters inside equations (Blackfriday bug workaround)
-(defun org-jekyll-md-escape-chars-in-equation (str)
+(defun org-jekyll-lite-escape-chars-in-equation (str)
   "Escape few characters in STR so that Markdown doesn't parse them.
 
 Adapted from ox-blackfriday via ox-hugo.
@@ -312,7 +312,7 @@ and finally:
 ;;; Keep the level as defined in original content
 ;;; ** subtree => ## heading
 
-(defun org-jekyll-md-headline-offset (headline contents info)
+(defun org-jekyll-lite-headline-offset (headline contents info)
   "proper headline offset"
   (let* ((info (plist-put info :headline-offset 0)))
     (org-md-headline headline contents info)))
@@ -320,7 +320,7 @@ and finally:
 
 ;;; Internal Filters
 
-(defun org-jekyll-md-separate-elements (tree _backend info)
+(defun org-jekyll-lite-separate-elements (tree _backend info)
   "Fix blank lines between elements.
 
 TREE is the parse tree being exported.  BACKEND is the export
@@ -355,35 +355,35 @@ Assume BACKEND is `jekyll'."
   ;; Return updated tree.
   tree)
 
-(defun org-jekyll-md-clean-language (language)
+(defun org-jekyll-lite-clean-language (language)
   "Clean up the language tag from a source block.
 
 At the moment, only translate 'ipython' to 'python'."
   (if (string= language "ipython") "python" language))
 
-(defun org-jekyll-md-src-block (src-block contents info)
+(defun org-jekyll-lite-src-block (src-block contents info)
   "Transcode SRC-BLOCK element into Markdown format. CONTENTS is
 nil.  INFO is a plist used as a communication channel.
 
 Adapted from ox-gfm."
-  (let* ((lang (org-jekyll-md-clean-language
+  (let* ((lang (org-jekyll-lite-clean-language
                 (org-element-property :language src-block)))
          (code (org-export-format-code-default src-block info))
          (prefix (concat "```" lang "\n"))
          (suffix "```"))
     (concat prefix code suffix)))
 
-(defun org-jekyll-md-table (table contents info)
+(defun org-jekyll-lite-table (table contents info)
   "Empty transformation. Org tables should be valid kramdown syntax."
   contents)
 
-(defun org-jekyll-md-table-cell (table-cell contents info)
+(defun org-jekyll-lite-table-cell (table-cell contents info)
   "Empty transformation. Org tables should be valid kramdown syntax."
   (if contents
       (format "| %s " contents)
     "| "))
 
-(defun org-jekyll-md-table-row (table-row contents info)
+(defun org-jekyll-lite-table-row (table-row contents info)
   "Empty transformation. Org tables should be valid kramdown syntax."
   (if contents
       (format "%s|" contents)
@@ -394,16 +394,16 @@ Adapted from ox-gfm."
 
 ;;; Template
 
-(defun org-jekyll-md-template (contents info)
+(defun org-jekyll-lite-template (contents info)
   "Return complete document string after MD conversion.
 
 CONTENTS is the transcoded contents string. INFO is a plist
 holding export options."
-  (if org-jekyll-md-include-yaml-front-matter
-      (concat (org-jekyll-md--yaml-front-matter info) contents)
+  (if org-jekyll-lite-include-yaml-front-matter
+      (concat (org-jekyll-lite--yaml-front-matter info) contents)
     contents))
 
-(defun org-jekyll-md-inner-template (contents info)
+(defun org-jekyll-lite-inner-template (contents info)
   "Return body of document string after MD conversion.
 
 CONTENTS is the transcoded contents string.  INFO is a plist
@@ -416,33 +416,33 @@ holding export options."
    contents
    ;; Footnotes section.
    "\n\n<!----- Footnotes ----->\n\n"
-   (org-jekyll-md-footnote-section info)))
+   (org-jekyll-lite-footnote-section info)))
 
 ;;; YAML Front Matter
 
-(defun org-jekyll-md--get-option (info property-name &optional default)
+(defun org-jekyll-lite--get-option (info property-name &optional default)
   "Get org export options, or an (optional) user-provided
 default, or (if user does not provide a default) an empty
 string."
   (let ((property (org-export-data (plist-get info property-name) info)))
     (format "%s" (or property default ""))))
 
-(defun org-jekyll-md--yaml-front-matter (info)
+(defun org-jekyll-lite--yaml-front-matter (info)
   "Creat YAML frontmatter content."
   (let ((convert-to-yaml-list
          (lambda (arg)
            (mapconcat #'(lambda (text)(concat "\n- " text))
                       (split-string arg) " "))))
     (let* ((layout-value
-            (org-jekyll-md--get-option info
-                                       :jekyll-layout org-jekyll-md-layout))
+            (org-jekyll-lite--get-option info
+                                       :jekyll-layout org-jekyll-lite-layout))
            (title
             (concat "\ntitle: \""
-                    (org-jekyll-md--get-option info
+                    (org-jekyll-lite--get-option info
                                                :title) "\""))
            (excerpt
             (concat "\nexcerpt: \""
-                    (org-jekyll-md--get-option info
+                    (org-jekyll-lite--get-option info
                                                :subtitle) "\""))
            ;; don't include the layout category if it's not specified
            (layout (if (not (string= layout-value ""))
@@ -451,19 +451,19 @@ string."
            (categories
             (concat "\ncategories: "
                     (funcall convert-to-yaml-list
-                             (org-jekyll-md--get-option
+                             (org-jekyll-lite--get-option
                               info
-                              :jekyll-categories org-jekyll-md-categories))))
+                              :jekyll-categories org-jekyll-lite-categories))))
            (tags
             (concat "\ntags: "
                     (funcall convert-to-yaml-list
-                             (org-jekyll-md--get-option
+                             (org-jekyll-lite--get-option
                               info
-                              :jekyll-tags org-jekyll-md-tags))))
+                              :jekyll-tags org-jekyll-lite-tags))))
            (date
             (and (plist-get info :with-date)
                  (concat "\ndate: "
-                         (org-jekyll-md--get-option info :date)))))
+                         (org-jekyll-lite--get-option info :date)))))
       (concat
        "---"
        title
@@ -477,21 +477,21 @@ string."
 ;;; End-User functions
 
 ;;;###autoload
-(defun org-jekyll-md-export-as-md (&optional async subtreep visible-only)
+(defun org-jekyll-lite-export-as-md (&optional async subtreep visible-only)
   "Export current buffer as a Markdown buffer adding some YAML front matter."
   (interactive)
   (org-export-to-buffer 'jekyll "*Org Jekyll-Markdown Export*"
     async subtreep visible-only nil nil (lambda () (markdown-mode))))
 
 ;;;###autoload
-(defun org-jekyll-md-export-to-md (&optional async subtreep visible-only)
+(defun org-jekyll-lite-export-to-md (&optional async subtreep visible-only)
   "Export current buffer to a Markdown file adding some YAML front matter."
   (interactive)
   (let ((outfile (org-export-output-file-name ".md" subtreep)))
     (org-export-to-file 'jekyll outfile async subtreep visible-only)))
 
 ;;;###autoload
-(defun org-jekyll-md-publish-to-md (plist filename pub-dir)
+(defun org-jekyll-lite-publish-to-md (plist filename pub-dir)
   "Publish an org file to Markdown with YAML front matter.
 
 FILENAME is the filename of the Org file to be published.  PLIST
@@ -502,13 +502,13 @@ Return output file name."
   (org-publish-org-to 'jekyll filename ".md" plist pub-dir))
 
 ;;;###autoload
-(defun org-jekyll-md-insert-export-options-template
+(defun org-jekyll-lite-insert-export-options-template
   (&optional title date setupfile categories tags layout)
   "Insert a settings template for Jekyll exporter."
   (interactive)
-  (let ((layout     (or layout org-jekyll-md-layout))
-        (tags       (or tags org-jekyll-md-tags))
-        (categories (or categories org-jekyll-md-categories)))
+  (let ((layout     (or layout org-jekyll-lite-layout))
+        (tags       (or tags org-jekyll-lite-tags))
+        (categories (or categories org-jekyll-lite-categories)))
     (save-excursion
       (insert (format (concat
                        "#+TITLE: "               title
@@ -521,6 +521,6 @@ Return output file name."
 
 ;;; provide
 
-(provide 'ox-jekyll-md)
+(provide 'ox-jekyll-lite)
 
-;;; ox-jekyll-md.el ends here
+;;; ox-jekyll-lite.el ends here
